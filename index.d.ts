ab5717cc941b7f99a3d9e5630017af47d0b3aab3
Mikhail Novikov <10743009+notiv-nt@users.noreply.github.com>
2023-04-26 12:34:54 +0300

    Add props

--- index.d.ts
+++ index.d.ts
@@ -2,12 +2,13 @@ import type { OptimizeOptions } from 'svgo'
 import type { Plugin } from "vite"
 
 type Options = {
-	svgoConfig: OptimizeOptions
+	svgoConfig?: OptimizeOptions
 	/**
 	 * Requires the use of ".svg?component" or ".svg?c" instead of just ".svg"
 	 * @default true
 	 */
 	requireSuffix: boolean
+	props?: string;
 }
 
 export default function(options?: Options): Plugin
--- index.js
+++ index.js
@@ -16,14 +16,17 @@ function compileSvg(source, filename, ssr) {
 
 function optimizeSvg(content, path, config = {}) {
   const { data } = optimize(content, {
-    ...config,
+    ...(config || {}),
     path,
   });
   return data;
 }
 
-module.exports = (options = {}) => {
-  const { svgoConfig, requireSuffix = true } = options;
+module.exports = (props = {}) => {
+  let requireSuffix = true;
+  if ('requireSuffix' in props) {
+    requireSuffix = props.requireSuffix;
+  }
   // Matches `*.svg`, `*.svg?component`, `*.svg?c`
   const svgRegex = /\.svg(?:\?(component))?(?:\?(c))?$/;
   const splitRegex = /(<svg.*?)(\/?>.*)/;
@@ -37,7 +40,7 @@ module.exports = (options = {}) => {
           .replace(".svg?component", ".svg")
           .replace(".svg?c", ".svg");
         const code = fs.readFileSync(idWithoutQuery);
-        let svg = optimizeSvg(code, idWithoutQuery, svgoConfig);
+        let svg = optimizeSvg(code, idWithoutQuery, props?.svgoConfig);
         // Support any custom attributes
         const parts = splitRegex.exec(svg);
         if (parts === null) {
@@ -47,7 +50,11 @@ module.exports = (options = {}) => {
           );
         } else {
           const [_, head, body] = parts;
-          svg = `${head} {...$$props}${body}`;
+          let someProps = '';
+          if (props?.props) {
+           someProps += props?.props;
+          }
+          svg = `${head} ${someProps} {...$$props}${body}`;
         }
         // Compile with Svelte
         return compileSvg(svg, idWithoutQuery, options?.ssr);
